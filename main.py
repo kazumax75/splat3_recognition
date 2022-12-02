a049dfc367d41b049896755eb0f978079077bde8
kazuma_kikuya <61053639+kazumax75@users.noreply.github.com>
2022-12-03 07:01:13 +0900

    Update main.py
    公開用に一部変更

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import cv2
 import splat_recognition as de
 
-import random, string
-import os
-import collections
-
 if __name__ == '__main__':
     writingFlag = False
     write_count = 0
     dir_name = ""
 
     wait = 0
-    # cap = cv2.VideoCapture(0, cv2.CAP_MSMF);cap.set(cv2.CAP_PROP_FPS, 60);wait=1
-    cap = cv2.VideoCapture(r'./assets/ready.avi')
+    cap = cv2.VideoCapture(0, cv2.CAP_MSMF);cap.set(cv2.CAP_PROP_FPS, 60);wait=1
+    # cap = cv2.VideoCapture(r'./assets/ready.avi')
 
     while cap.isOpened():
         # キャプチャボードから画像取得
@@ -30,8 +26,7 @@ if __name__ == '__main__':
                 de.アイコンの位置検出と取得(frame, frame2)
             
             de.プレイヤー状況を画面に表示する()
-            # cv2.imshow("game", frame)
-            cv2.imshow("game2", frame2)
+            cv2.imshow("game", frame2)
         
             if writingFlag:
                 fn = r"./capture/%s/%d.bmp" % (dir_name, write_count)
@@ -43,17 +38,6 @@ if __name__ == '__main__':
         elif key == ord('q'):
             #qキーで終了
             break
-        elif key == 32:
-            if writingFlag:
-                print("録画停止。")
-                writingFlag = False
-                write_count = 0
-                
-            elif writingFlag == False and ret :
-                print("録画開始！")
-                writingFlag = True
-                dir_name = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
-                os.makedirs('./capture/'+dir_name, exist_ok=True)
                 
 
     # メモリを解放して終了
